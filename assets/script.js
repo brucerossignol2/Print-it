0045e6971d8a5bcd2428698bbd4c85ada4f3eaa6
BruceRossignol <rossignol.bruce@gmail.com>
2025-01-28 12:29:52 +0100

    Etape 3: Ajout des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,22 @@ const slides = [
 
 const flecheGauche = document.getElementById("flecheGauche");
 const flecheDroite = document.getElementById("flecheDroite");
+let selectedIndex = 0; 	// Index du point sélectionné (commence à 0)
+const nombreDeSlides = slides.length; 	// Compte le nombre d'entrées
+console.log(`Nombre d'entrées dans le tableau : ${nombreDeSlides}`);
+const dots = document.querySelector(".dots");	// Conteneur des points
+
+for (let i = 0; i < nombreDeSlides; i++) {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+  
+	// Ajouter la classe "dot_selected" au point sélectionné
+	if (i === selectedIndex) {
+	  dot.classList.add("dot_selected");
+	}
+	// Ajouter chaque point au conteneur
+	dots.appendChild(dot);
+  }
 
 // écoute les clics sur les flêches
 flecheGauche.addEventListener("click", () => {
